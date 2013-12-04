0b456dfae1307a094a267f925848f25e089f38f3
Imanol Cea <imanol.cea@gmail.com>
2013-12-04 11:59:23 +0100

    Add views & controller for report generator

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,8 @@ Iom::Application.routes.draw do
     match '/' => 'admin#index', :as => :admin
     match '/export_projects' => 'admin#export_projects', :as => :export_projects
     match '/export_organizations' => 'admin#export_organizations', :as => :export_organizations
+    match '/report' => 'reports#index', :as => :report_index
+    match '/report_generate' => 'reports#report', :as => :report_generate
     resources :settings, :only => [:edit, :update]
     resources :users
     resources :tags, :only => [:index]
