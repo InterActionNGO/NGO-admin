d13374d3fb58a857e370c2225f4ec1fe2ad1bcf1
Imanol Cea <imanol.cea@gmail.com>
2014-01-08 18:58:31 +0100

    Add backoffice layers CRUD

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -6,19 +6,21 @@
 #  title          :string(255)     
 #  description 	  :text            
 #  credits     	  :text 
-#  sql			  :text           
 #  date	       	  :date            
 #  min       	  :integer            
 #  max       	  :integer            
 #  units       	  :integer            
 #  status      	  :boolean    
 #  cartodb_table  :string     
+#  sql			  :text           
 #
 
 class Layer < ActiveRecord::Base
   # attr_accessible :site_layers_attributes
   has_many  :site_layers
   has_many  :site, :through => :site_layers
+
+  validates_presence_of :title, :cartodb_table, :sql, :min, :max
   # accepts_nested_attributes_for :site_layers
 
   # to get only id and name
