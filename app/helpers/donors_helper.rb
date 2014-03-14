a8d6166d7049aabfa641a328c54e0e06a2e9d637
Miguel Mendoza <info@miguelmendoza.dj>
2014-03-14 17:37:21 +0100

    Adds one more title case

--- app/helpers/donors_helper.rb
+++ app/helpers/donors_helper.rb
@@ -4,6 +4,8 @@ module DonorsHelper
     by = "FUNDED BY #{@donor.name}"
     if @filter_by_location && @filter_by_category && @filter_by_organization
       pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{@organization.name}" + " in #{@location_name}"
+    elsif @filter_by_location && @filter_by_organization
+      pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{@organization.name}"
     elsif @filter_by_location && @filter_by_category
       pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " in #{@location_name}"
     elsif @filter_by_location
