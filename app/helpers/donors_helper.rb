02eb90dee1fa5c32eaedf6ceb8e24dfd14dbafd6
Miguel Mendoza <info@miguelmendoza.dj>
2014-03-18 17:23:43 +0100

    adds category name

--- app/helpers/donors_helper.rb
+++ app/helpers/donors_helper.rb
@@ -8,18 +8,25 @@ module DonorsHelper
     end
     if @filter_by_location && @filter_by_category && @filter_by_organization
       pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{@organization.name}" + " in #{@location_name}"
+
     elsif @filter_by_category && @filter_by_organization
       pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{@organization.name}"
+  
     elsif @filter_by_location && @filter_by_organization
       pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{@organization.name}" + " in #{@location_name}"
+   
     elsif @filter_by_location && @filter_by_category
       pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " in #{@location_name}"
+   
     elsif @filter_by_location
       pluralize(@projects_count, "PROJECT", "PROJECTS") + ' ' + by + " in #{@location_name}"
+   
     elsif @filter_by_category
       pluralize(@projects_count, "#{@filter_name} PROJECT", "#{@filter_name} PROJECTS") + ' ' + by
+    
     elsif @filter_by_organization
       pluralize(@projects_count, "PROJECT", "PROJECTS") + ' ' + by + " implemented  by #{@organization.name}" 
+    
     else
       pluralize(@projects_count, "PROJECT", "PROJECTS") + ' ' + by
     end
