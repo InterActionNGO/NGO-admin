58844acc2ae36ebaa7c3457a04b620d825ebb62e
Miguel Mendoza <info@miguelmendoza.dj>
2014-04-04 11:33:05 +0200

    unescapeHTML orgs names

--- app/helpers/donors_helper.rb
+++ app/helpers/donors_helper.rb
@@ -4,16 +4,16 @@ module DonorsHelper
     if header
       by = ""
     else
-      by = "FUNDED BY #{@donor.name}"
+      by = "FUNDED BY #{CGI.unescapeHTML(@donor.name)}"
     end
     if @filter_by_location && @filter_by_category && @filter_by_organization
-      pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{@organization.name}" + " in #{@location_name}"
+      pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{CGI.unescapeHTML(@organization.name)}" + " in #{@location_name}"
 
     elsif @filter_by_category && @filter_by_organization
-      pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{@organization.name}"
+      pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{CGI.unescapeHTML(@organization.name)}"
   
     elsif @filter_by_location && @filter_by_organization
-      pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{@organization.name}" + " in #{@location_name}"
+      pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " implemented  by #{CGI.unescapeHTML(@organization.name)}" + " in #{@location_name}"
    
     elsif @filter_by_location && @filter_by_category
       pluralize(@projects_count, "#{@category_name} PROJECT", "#{@category_name} PROJECTS") + ' ' + by + " in #{@location_name}"
@@ -25,7 +25,7 @@ module DonorsHelper
       pluralize(@projects_count, "#{@filter_name} PROJECT", "#{@filter_name} PROJECTS") + ' ' + by
     
     elsif @filter_by_organization
-      pluralize(@projects_count, "PROJECT", "PROJECTS") + ' ' + by + " implemented  by #{@organization.name}" 
+      pluralize(@projects_count, "PROJECT", "PROJECTS") + ' ' + by + " implemented  by #{CGI.unescapeHTML(@organization.name)}" 
     
     else
       pluralize(@projects_count, "PROJECT", "PROJECTS") + ' ' + by
