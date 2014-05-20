478be9612ac639dbb9ffd790006cd8981fcdb70f
David Inga <david.inga@simbiotica.es>
2014-05-20 16:10:40 +0200

    report

---
+++ public/app/javascripts/views/report-results.js
@@ -0,0 +1,48 @@
+'use strict';
+
+define([
+  'backbone',
+  'handlebars',
+  'text!../../templates/report.handlebars'
+], function(Backbone, Handlebars, tpl) {
+
+  var TotalsView = Backbone.View.extend({
+
+    el: '#totalsView',
+
+    template: Handlebars.compile(tpl),
+
+    initialize: function() {
+      Backbone.Events.on('results:empty', this.empty, this);
+      this.model.on('change', this.render, this);
+    },
+
+    render: function() {
+      console.log(this.model.toJSON());
+      this.$el.html(this.template(this.model.toJSON()));
+      this.calculeReportBudget();
+    },
+
+    empty: function() {
+      this.$el.html('');
+    },
+
+    calculeReportBudget: function() {
+      var $budgetChart = $('#reportBudgetChart');
+      var min = $budgetChart.data('min');
+      var max = $budgetChart.data('max');
+      var average = $budgetChart.data('average');
+      var total = Number(min) + Number(max) + Number(average);
+      var w = $budgetChart.width() - 2;
+
+      $budgetChart
+        .append('<div class="mod-report-budget-chart-item min" style="width: ' + ((min * w) / total).toFixed(0) + 'px">')
+        .append('<div class="mod-report-budget-chart-item average" style="width: ' + ((average * w) / total).toFixed(0) + 'px">')
+        .append('<div class="mod-report-budget-chart-item max" style="width: ' + ((max * w) / total).toFixed(0) + 'px">');
+    }
+
+  });
+
+  return TotalsView;
+
+});
