071a770dfae2e5ee82f1ebcf43c69cad09cf9333
David Inga <david.inga@simbiotica.es>
2014-06-04 17:28:38 +0200

    staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -49,4 +49,17 @@ Iom::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_controller.asset_path = proc { |asset_path|
+    case asset_path
+    when /^\/(system)/
+      "#{asset_path}"
+    when /^\/(stylesheets\/lib)/
+      "/app#{asset_path}".sub("/stylesheets", "")
+    when /^\/(stylesheets\/vendor)/
+      "/app#{asset_path}".sub("/stylesheets", "")
+    else
+      "/dist#{asset_path}"
+    end
+  }
+
 end
