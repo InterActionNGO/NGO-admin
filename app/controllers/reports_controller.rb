e603b5c2b7c9782a5d33348ad8f9c76a9cc7e87e
David Inga <david.inga@simbiotica.es>
2014-08-25 13:14:33 +0200

    removed all option [#77528848]

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,10 +8,10 @@ class ReportsController < ApplicationController
 		respond_to do |format|
 			format.html do
 				#render :html => '/reports/index'
-				@org_combo_values = [['All','All']] + Organization.get_select_values.collect{ |o| [o.name, o.name] }
-				@countries_combo_values = [['All','All']] + Country.get_select_values.collect{ |c| [c.name, c.name] }
-				@sectors_combo_values = [['Any sector','All']] + Sector.get_select_values.collect { |c| [c.name, c.name] }
-				@donors_combo_values = [['Any donor','All']] + Donor.get_select_values.collect{ |d| [d.name, d.name] }
+				@org_combo_values = Organization.get_select_values.collect{ |o| [o.name, o.name] }
+				@countries_combo_values = Country.get_select_values.collect{ |c| [c.name, c.name] }
+				@sectors_combo_values = Sector.get_select_values.collect { |c| [c.name, c.name] }
+				@donors_combo_values = Donor.get_select_values.collect{ |d| [d.name, d.name] }
 				@date_start = Date.new(2004, 9, 1)
 				@date_end = Date.today
 			end
