86b7893c9f929a12e445c2b4ed6a25821c35f20c
David Inga <david.inga@icloud.com>
2014-08-28 16:19:08 +0200

    show list

--- public/app/javascripts/report/views/class/lists.js
+++ public/app/javascripts/report/views/class/lists.js
@@ -43,17 +43,32 @@ define([
 
     _showList: function(list) {
       var items = ReportModel.instance.get(list.name);
+      var result = [];
+      var rLen = 0;
+      var iLen = 0;
+
+      this.currentList = list;
 
       if (list.name === this.options.slug) {
-        console.log(list.category);
         this.data = {};
-        this.data[this.options.slug] = _.first(_.sortBy(items, function(item) {
+        result = _.sortBy(items, function(item) {
           if (typeof item[list.category] === 'string') {
             return item[list.category];
           }
           return -item[list.category];
-        }), this.options.limit);
-        console.log(this.data[this.options.slug]);
+        });
+
+        if (this.options.limit) {
+          result = _.first(result, this.options.limit);
+        }
+
+        iLen = items.length;
+        rLen = result.length;
+
+        this.data[this.options.slug] = result;
+        this.data.title = (!this.options.limit || iLen < this.options.limit) ? null : rLen;
+        this.data.isLong = iLen > result.length;
+        this.data.isActive = this.isShowAllActive;
         this.render();
         _.delay(_.bind(this.show, this), 200);
       }
@@ -71,15 +86,13 @@ define([
 
     _toggleShowAll: function() {
       if (this.isShowAllActive) {
+        this.options.limit = 30;
         this.isShowAllActive = false;
-        this._showList({
-          name: this.options.slug,
-          category: ''
-        });
       } else {
         this.isShowAllActive = true;
-        _.delay(_.bind(this.show, this), 200);
+        this.options.limit = null;
       }
+      this._showList(this.currentList);
     },
 
     _onClickSelector: function(e) {
