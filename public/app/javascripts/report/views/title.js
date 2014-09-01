625b5b60f127f620672ef44f82b43d380e1d1468
David Inga <david.inga@simbiotica.es>
2014-09-01 13:18:49 +0200

    report title

--- public/app/javascripts/report/views/title.js
+++ public/app/javascripts/report/views/title.js
@@ -15,12 +15,31 @@ define([
     initialize: function() {
       this.$textarea = this.$el.find('textarea');
       this.autoResizeTextarea();
+      this.checkStringWidth();
       Backbone.Events.on('filters:done', this.showFilters, this);
+    },
+
+    onKeyUp: function() {
+      this.checkStringWidth();
+      this.autoResizeTextarea();
     },
 
     autoResizeTextarea: function() {
       this.$textarea.css('height', 0)
         .height(this.$textarea[0].scrollHeight);
+    },
+
+    checkStringWidth: function() {
+      var len = this.$textarea.val().length;
+      var w = 275;
+
+      if (len === 0 || len < 15) {
+        w = 275;
+      } else {
+        w = '90%';
+      }
+
+      this.$textarea.width(w);
     }
 
   });
