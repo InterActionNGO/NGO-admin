5993d8b770a816fca4dc834c8eb78f7a20e110f6
David Inga <david.inga@simbiotica.es>
2014-09-01 12:04:39 +0200

    profile template

--- public/app/javascripts/report/views/organizations-snapshot.js
+++ public/app/javascripts/report/views/organizations-snapshot.js
@@ -40,7 +40,7 @@ define([
       var organizations = ReportModel.instance.get('organizations');
       var len = organizations.length;
 
-      this.data = this.profile = {};
+      this.data = this.profile = null;
 
       if (len > 1) {
 
@@ -113,7 +113,7 @@ define([
             type: 'column',
             spacingLeft: 0,
             spacingRight: 0,
-            width: 206,
+            width: 188,
             reflow: false
             // height: (this.data.charts[index].series.length > 3) ? 600 : 250
           },
