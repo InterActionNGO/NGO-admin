50e1c0e19376ad296740bfcede22f5ce17de7391
Miguel Mendoza <info@miguelmendoza.dj>
2015-10-20 10:55:49 +0200

    changes deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
+require "rvm/capistrano"
 set :default_stage, "production"
-
 role :app, linode_production
 role :web, linode_production
+set :use_sudo, false
 role :db,  linode_production, :primary => true
-
-set :branch, "master"
-
+set :rvm_type, :system
+set :branch, fetch(:branch, "iati-refactor")
 task :set_staging_flag, :roles => [:app] do
 end
