a0b686752d43606e1b847dfcc5fc67e03c3dce06
Megan Zlock <zlockart@gmail.com>
2016-08-26 18:22:26 -0400

    Add field for adding images to projects by url, not uploading

--- public/app/javascripts/backoffice/media.js
+++ public/app/javascripts/backoffice/media.js
@@ -6,25 +6,4 @@
       $(this).parent().hide();
       $(this).parent().parent().find('div.caption').show();
     });
-
-    //remove text input
-    $('input.video').focusin(function(ev){
-      var value = $(this).attr('value');
-      if (value == "Enter a Vimeo or Youtube URL") {
-        $(this).attr('value','');
-      }
-    });
-    $('input.video').focusout(function(ev){
-      var value = $(this).attr('value');
-      if (value == "") {
-        $(this).attr('value',"Enter a Vimeo or Youtube URL");
-      }
-    });
-
-    //add video or image
-    $('.toggle_image_video').click(function(){
-      $('#new_image').toggle();
-      $('#new_video').toggle();
-      return false;
-    });
   });
