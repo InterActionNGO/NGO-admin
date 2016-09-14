d8b52bf2d42af2519f0a918e1e5ab9641116b807
Brian Landau <brianjlandau@gmail.com>
2016-09-14 17:48:23 -0600

    Get grunt running hopefully

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :user, "deploy"
 set :deploy_via, :remote_cache
 
 set :default_environment, {
-  'PATH' => '/opt/rbenv/shims:/usr/local/rbenv/shims:/opt/rbenv/bin:/usr/local/rbenv/bin:$PATH'
+  'PATH' => '/opt/rbenv/shims:/usr/local/rbenv/shims:/opt/rbenv/bin:/usr/local/rbenv/bin:/usr/local/nvm/versions/node/v4.3.1/bin:$PATH'
 }
 
 set(:deploy_to)  { "/var/www/#{application}/#{rails_env}" }
