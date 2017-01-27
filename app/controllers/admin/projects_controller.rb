cb7fc7bd791321c529ad20636f4776acd79a686c
Brian Landau <brianjlandau@gmail.com>
2017-01-27 03:01:22 -0700

    Fix project create issues. see #39

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -93,8 +93,6 @@ class Admin::ProjectsController < Admin::AdminController
     else
       @organizations_ids   = organizations_ids
       @countries_iso_codes = countries_iso_codes
-      @countries = @project.country_ids.map{|id| Country.find(id)}
-      @regions = @project.region_ids.map{|id| Region.find(id)}
       render :action => 'new'
     end
   end
