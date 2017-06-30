75b5706147090edfe21e2968e32e1919d48c3ec7
Robert Burnett <robert@opuselegans.com>
2017-06-30 16:15:31 -0400

    troubleshooting an error with early geolocations migration

--- db/migrate/20170630144854_add_geolocation_indexes.rb
+++ db/migrate/20170630144854_add_geolocation_indexes.rb
@@ -1,11 +1,9 @@
 class AddGeolocationIndexes < ActiveRecord::Migration
   def self.up
-      add_index :geolocations, :id, :unique => true
       add_index :geolocations, :readable_path
   end
 
   def self.down
-      remove_index :geolocations, :id
       remove_index :geolocations, :readable_path
   end
 end
