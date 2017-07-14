f2f48cdc689042986309cbf8a23b71b36fee861d
Robert Burnett <robert@opuselegans.com>
2017-07-14 13:20:05 -0400

    add fuzzy matching to organization index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170630144854) do
+ActiveRecord::Schema.define(:version => 20170714154027) do
 
   create_table "changes_history_records", :force => true do |t|
     t.integer  "user_id"
