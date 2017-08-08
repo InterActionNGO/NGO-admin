9753e8f0ef0d7dca17c197047c9dac00c34e6863
Robert Burnett <robert@opuselegans.com>
2017-08-08 14:52:24 -0400

    add sync all function IATI module. add rake tast. set whenever schedule. add mailer.

--- lib/iati_registry.rb
+++ lib/iati_registry.rb
@@ -16,6 +16,128 @@ module IATI
         PUBLISHER_EMAIL = Rails.configuration.iati_publisher_email
         IATI_STANDARD_VERSION = Rails.configuration.iati_standard_version
         
+        class Sync
+            
+            def self.all
+                result = {
+                    :add => { :total => queue[:add].size, :errors => add[:fail] },
+                    :remove => { :total => queue[:remove].size, :errors => remove[:fail] },
+                    :update => { :total => queue[:update].size, :errors => update[:fail] }
+                }
+                IatiMailer.sync(PUBLISHER_EMAIL, result).deliver
+                result
+            end
+            
+            def self.queue(return_registry_objects=true)
+                {   :add => Organization.iati_eligible.has_projects - parent.publisher_datasets,
+                    :remove => parent.publisher_datasets - Organization.iati_eligible,
+                    :update => calculate_updates(return_registry_objects)
+                }
+            end
+            
+            def self.add(orgs=queue[:add])
+                results = {:success => [], :fail => []}
+                orgs.each do |org|
+                    process_response(parent.package_create(parent::Package.new(org).data), results)
+                end
+                results
+            end
+            
+            def self.remove(orgs=queue[:remove])
+                results = {:success => [], :fail => []}
+                orgs.each do |org|
+                    process_response(parent.package_delete(NAME_PREFIX + org.id.to_s), results)
+                end
+                results
+            end
+            
+            def self.update(orgs=queue[:update])
+                results = {:success => [], :fail => []}
+                orgs.each do |org|
+                   process_response(parent.package_update(parent::Package.new(org).data), results) 
+                end
+                results
+            end
+            
+            def self.process_response(request, resultHash)
+                if request.code.eql?(200)
+                    resultHash[:success] << request
+                else
+                    resultHash[:fail] << request
+                end
+            end
+            
+            private
+                
+            # TODO Set up touch option on Organization.projects so modifications to an organization's projects updates the Organization's modified attribute. This will catch project deletions that cannot be captured by the below method
+            def self.calculate_updates(return_registry_objects=true)
+                registry = JSON.parse(parent.publisher_datasets(:mapped => false))['result']['results']
+                queue = []
+                registry.each do |reg|
+                    id = reg['url'].split('/').last
+                    nam = Organization.find(id)
+                    obj = return_registry_objects.eql?(true) ? reg : nam
+                    if nam.projects.size > 0
+                        registry_freshest = DateTime.parse(reg['resources'].first['last_modified'])
+                        ngoaidmap_freshest = DateTime.parse(Project.organizations(nam.id).order('updated_at desc').first.updated_at.strftime("%FT%T"))
+                        queue << obj if (ngoaidmap_freshest - registry_freshest) > 0
+                    end
+                end
+                queue
+            end
+        end
+        
+        class << self
+            
+            def publisher_datasets(options={})
+                options = {:org => PUBLISHER, :mapped => true}.merge(options)
+                
+                # This method is not designed to page through result sets greater than 1000
+                params = {:rows=>1000, :q=>"organization:#{options[:org]}"}
+                
+                results = Rails.cache.fetch("package_search/#{params.to_s}", :compress => true, :expires_in => 12.hours) do
+                    package_search(params).to_json
+                end
+                
+                ids = []
+                JSON.parse(results)['result']['results'].each do |r|
+                   ids << r['url'].split('/').last.to_i 
+                end
+                if options[:mapped].eql?(true)
+                    Organization.where(:id => ids).order(:name)
+                else
+                    results
+                end
+            end
+            
+            
+            protected
+            
+            def package_create(pkg={})
+                self.post("/package_create", {:body => pkg.to_json, :headers => headers}) 
+            end
+            
+            def package_search(params={})
+                self.get("/package_search", :query => params) 
+            end
+            
+            def package_delete(id)
+                self.post("/package_delete", {:body => {:id => id}.to_json, :headers => headers }) unless id.nil?
+            end
+            
+            def package_update(pkg={})
+                self.post("/package_update", {:body => pkg.to_json, :headers => headers})
+            end
+            
+            private
+            def headers(additional={})
+                {
+                    'Authorization' => Rails.configuration.iati_api_key,
+                    'Content-Type' => 'application/json'
+                }.merge(additional)
+            end
+        end
+        
         class Package 
             
             def data 
@@ -25,6 +147,7 @@ module IATI
             def initialize(org)
                 
                 id = org['url'].split('/').last unless org.class.eql?(Organization)
+                
                 # If not Organization instance, create one
                 nam = org.class.eql?(Organization) ? org : Organization.find(id)
                 
@@ -64,121 +187,6 @@ module IATI
                 unless org.class == Organization
                     @object = org.merge(@object)
                 end
-                
-                
-            end
-            
-        end
-        
-        class Sync
-            
-            def self.queue
-                {   :add => Organization.iati_eligible.has_projects - parent.publisher_datasets,
-                    :remove => parent.publisher_datasets - Organization.iati_eligible,
-                    :update => calculate_updates
-                }
-            end
-            
-            def self.add(orgs=queue[:add])
-                # Takes an array of organization objects
-                results = {:success => [], :fail => []}
-                orgs.each do |org|
-                    process_response(parent.package_create(parent::Package.new(org).data), results)
-                end
-                results
-            end
-            
-            def self.remove(orgs=queue[:remove])
-                results = {:success => [], :fail => []}
-                orgs.each do |org|
-                    process_response(parent.package_delete(NAME_PREFIX + org.id.to_s), results)
-                end
-                results
-            end
-            
-            def self.update(orgs=queue[:update])
-                results = {:success => [], :fail => []}
-                orgs.each do |org|
-                   process_response(parent.package_update(parent::Package.new(org).data), results) 
-                end
-                results
-            end
-            
-            def self.process_response(request, resultHash)
-                if request.code.eql?(200)
-                    resultHash[:success] << request
-                else
-                    resultHash[:fail] << request
-                end
-            end
-            
-            private
-                
-            # TODO Set up touch option on Organization.projects so modifications to an organization's projects updates the Organization's modified attribute. This will catch project deletions that cannot be captured by the below method
-            def self.calculate_updates
-#                 registry = parent.publisher_datasets({:mapped => false}) - (parent.publisher_datasets - Organization.iati_eligible)
-                registry = JSON.parse(parent.publisher_datasets(:mapped => false))['result']['results']
-                queue = []
-                registry.each do |reg|
-                    id = reg['url'].split('/').last
-                    nam = Organization.find(id)
-                    if nam.projects.size > 0
-                        registry_freshest = DateTime.parse(reg['resources'].first['last_modified'])
-                        ngoaidmap_freshest = Project.organizations(nam.id).order('updated_at desc').first.updated_at
-                        queue << reg if (ngoaidmap_freshest - registry_freshest) > 0
-                    end
-                end
-                queue
-            end
-        end
-        
-        class << self
-            
-            def publisher_datasets(options={})
-                options = {:org => PUBLISHER, :mapped => true}.merge(options)
-                
-                # This method is not designed to page through result sets greater than 1000
-                params = {:rows=>1000, :q=>"organization:#{options[:org]}"}
-                
-                results = Rails.cache.fetch("package_search/#{params.to_s}", :compress => true, :expires_in => 1.day) do
-                    package_search(params).to_json
-                end
-                
-                ids = []
-                JSON.parse(results)['result']['results'].each do |r|
-                   ids << r['url'].split('/').last.to_i 
-                end
-                if options[:mapped].eql?(true)
-                    Organization.where(:id => ids).order(:name)
-                else
-                    results
-                end
-            end
-            
-            
-            protected
-            
-            def package_create(pkg={})
-                self.post("/package_create", {:body => pkg.to_json, :headers => headers}) 
-            end
-            
-            def package_search(params={})
-                self.get("/package_search", :query => params) 
-            end
-            
-            def package_delete(id)
-                self.post("/package_delete", {:body => {:id => id}.to_json, :headers => headers }) unless id.nil?
-            end
-            
-            def package_update(pkg={})
-                self.post("/package_update", {:body => pkg.to_json, :headers => headers})
-            end
-            
-            def headers(additional={})
-                {
-                    'Authorization' => Rails.configuration.iati_api_key,
-                    'Content-Type' => 'application/json'
-                }.merge(additional)
             end
         end
     end
