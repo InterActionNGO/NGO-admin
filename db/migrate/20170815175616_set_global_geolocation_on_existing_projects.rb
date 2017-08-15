dfdbe6eaab9aea6a3bebf1d017dc0926467c29e1
Robert Burnett <robert@opuselegans.com>
2017-08-15 14:00:39 -0400

    redo global geolocation migration

--- db/migrate/20170815175616_set_global_geolocation_on_existing_projects.rb
+++ db/migrate/20170815175616_set_global_geolocation_on_existing_projects.rb
@@ -1,6 +1,6 @@
 class SetGlobalGeolocationOnExistingProjects < ActiveRecord::Migration
   def self.up
-      # Set existing global scope projects with new global geolocation
+    # Set existing global scope projects with new global geolocation
       Project.where(:geographical_scope => 'global').each do |p|
           p.save!
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170815173710) do
+ActiveRecord::Schema.define(:version => 20170815175616) do
 
   create_table "changes_history_records", :force => true do |t|
     t.integer  "user_id"
