f9d41455fc18ad8c3db31132bddc9de0f487b8cb
David Eisinger <david.eisinger@viget.com>
2017-09-13 13:12:50 -0400

    Clean up scope form

--- app/models/humanitarian_scope_vocabulary.rb
+++ app/models/humanitarian_scope_vocabulary.rb
@@ -3,4 +3,9 @@ class HumanitarianScopeVocabulary < ActiveRecord::Base
             :code,
             :presence => true,
             :uniqueness => { :case_sensitive => false }
+
+  def to_s
+    "#{name} (#{code})"
+  end
+  alias :display_name :to_s
 end
