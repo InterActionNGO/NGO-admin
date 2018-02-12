781d876dbe6e349c1aa6dc4e9a4491080e656063
Robert Burnett <robert@opuselegans.com>
2018-02-12 14:19:10 -0500

    cap troubleshoot

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 # require "rvm/capistrano"
 # default_run_options[:pty] = true
 set :default_stage, "production"
-set :default_environment, {
-  'PATH' => '/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin/rbenv:/home/deploy/.nvm/v0.10.29/bin:$PATH'
-}
+# set :default_environment, {
+#   'PATH' => '/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin/rbenv:/home/deploy/.nvm/v0.10.29/bin:$PATH'
+# }
 role :app, "52.179.82.220"
 role :web, "52.179.82.220"
 role :db, "52.179.82.220"
