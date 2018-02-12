4f19e43062903ba21e01bd00afd88fd080da5837
Robert Burnett <robert@opuselegans.com>
2018-02-12 14:04:56 -0500

    add force to soft link in deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ task :symlinks, :roles => [:app] do
   run <<-CMD
     ln -s #{shared_path}/config/app_config.yml #{release_path}/config/app_config.yml;
     ln -s #{shared_path}/node_modules #{release_path}/node_modules;
-    ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml;
+    ln -sf #{shared_path}/config/database.yml #{release_path}/config/database.yml;
   CMD
 end
 
